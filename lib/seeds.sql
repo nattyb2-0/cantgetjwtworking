6c95e9826b63b5c435049cf32e7339e48f0b4a5f
Daniel Pease <daniel.j.pease@gmail.com>
2016-12-06 09:28:58 -0500

    improve seeds

--- lib/seeds.sql
+++ lib/seeds.sql
@@ -6,4 +6,10 @@ INSERT INTO users (user_id, username, password) VALUES
 (2, 'trevor', 'trevor'),
 (3, 'matt', 'matt');
 
+INSERT INTO secretStuff (secret_id, mySecret, myOtherSecret) VALUES
+
+(1, 'one time i stole', 'another time i lied'),
+(2, 'i have a phobia of squirrels', 'jk'),
+(3, 'third secret', 'need coffee to be witty');
+
 COMMIT;
